4a078b00980659e2a73f613f1dc19c4552757280
Pedrot79 <97770413+Pedrot79@users.noreply.github.com>
2022-02-18 17:28:35 -0700

    Add files via upload

---
+++ peticiones.js
@@ -0,0 +1,80 @@
+const cargarTipos = async()=>{
+    try{
+        const url ="tipos.json"
+        await axios
+        .get(url)
+        .then((res)=>{
+            llenarCombos(res.data);
+        })
+        .catch((err)=>{
+            console.log("Surgió un error en la petición");
+            return false; 
+        });
+    } catch(error){
+        console.log("Surgió un error");
+        return false;
+    }
+    return true;
+}
+
+function llenarCombos(data){
+    for(let item of data){
+        document.getElementById("tipo").innerHTML+=
+        `<option value="${item.tipo}">${item.descripcion}</option>`
+    }
+}
+
+const cargarTablaVentas = async()=>{
+    const urlVentas = "ventas.json"
+    try{
+        await axios
+        .get(urlVentas)
+        .then((res)=>{
+            llenarTabla(res.data);
+        })
+        .catch((err)=>{
+            console.log("Surgió un error en la petición");
+            return false;
+        });
+    } catch(err){
+        console.log("Surgió un error");
+        return false;
+    }
+    return true;
+}
+
+function llenarTabla(data){
+    //Limpiar tabla
+    document.getElementById("ventas").innerHTML=``;
+    //Sacar el tipo del combox
+    let tipo = document.getElementById("tipo").value;
+    for(let item of data){
+        if(item.tipo == tipo){
+            document.getElementById("ventas").innerHTML+=`<tr>
+                                                            <td>${item.folio}</td>
+                                                            <td>${item.tipo}</td>
+                                                            <td>${item.precio}</td>
+                                                            <td>${item.descuento}</td>
+                                                            <td>${item.total}</td>
+                                                            <td>${item.fechapago}</td>
+                                                            <td>${item.giro}</td>
+                                                        </tr>`;            
+        }
+    }
+}
+
+document.getElementById("limpiar").addEventListener("click", async()=>{
+    document.getElementById("ventas").innerHTML=``;
+});
+
+const inicia = async()=>{
+    if(await cargarTipos()==true){
+        cargarTablaVentas();
+    } else{
+        console.log("Surgió un error");
+    }
+}
+
+inicia();
+
+
